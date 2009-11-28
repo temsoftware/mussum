3cb9481948854cff1adb87d7d4095959668da231
Fernando Fujie / Eduardo Zaghi / Marco Singer <markaum@gmail.com>
2009-11-28 15:45:32 -0200

    nifty User

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "Successfully created user."
+      redirect_to @user
+    else
+      render :action => 'new'
+    end
+  end
+end
