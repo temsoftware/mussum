a6f2ff641d5c99a25edb63428e7ea8089475ba2f
alisonasoo <alison.asoo@gmail.com>
2009-11-28 16:57:26 -0200

    Ïniciando Rspec para Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :login, :pass
+  validates_presence_of :name, :login, :pass
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,15 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe User do
+  
+  
+  
   it "should be valid" do
-    User.new.should be_valid
+    User.create(:name => "alisonasoo", :login => "alisonasoo", :pass => "manchaverde").should be_valid
   end
+  
+  it "should be invalido without all parameters" do
+    User.create(:name => nil, :login => nil, :pass => nil).should be_invalid
+  end
+
 end
